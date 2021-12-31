fdd4d301271da1c84102cfaca8feb39182fa6285
eugenieC <40417846+eugenieC@users.noreply.github.com>
2021-12-31 11:04:32 -0800

    Delete Assignment2 directory

--- Assignment2/CS106a assignment2 - part1.3.liftoff.py
+++
@@ -1,9 +0,0 @@
-# prints out the calls for a spaceship that is about to launch. Countdown the numbers from 10 to 1 and then write “Liftoff!”
-
-def main():
-  for x in range(10, 0,-1):
-    print(x)
-  print("Liftoff!")
-
-if __name__ == "__main__":
- main()
